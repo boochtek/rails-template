36cb75bce445dfe717f4d38cbee41cce90161dd0
Craig Buchek <craig@boochtek.com>
2009-08-09 16:40:14 -0500

    Add 'hasJS' class to BODY if the user has JavaScript enabled.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,9 @@
+// Allow CSS to easily determine whether we're running with JavaScript enabled or not.
+jQuery('body').addClass('hasJS'); // Considered adding the class to HEAD, but while the DOM supports it, the HTML spec doesn't allow className on HEAD.
+
 // This section will run after all the DOM is loaded (i.e. all elements) but right before they're displayed, and before all the images and such are loaded.
 jQuery(document).ready(function(){
+
     // Focus on the first text field. NOTE: The focusOnFirstInvalidField option to enableValidation() can override this.
     jQuery(':text:first').focus(); // TODO: May want to specify a specific form or field you want to focus on.
 
