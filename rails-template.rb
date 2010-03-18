62c593e7cf7cef80c79e12de6ca62a2f8d586c48
Craig Buchek <craig@boochtek.com>
2010-03-17 23:05:16 -0500

    Formtastic now uses the "formtastic" generator instead of "formtastic_stylesheets".

--- rails-template.rb
+++ rails-template.rb
@@ -289,7 +289,7 @@ end
 plugin 'validation_reflection', :git => 'http://github.com/redinger/validation_reflection.git', :submodule => true
 plugin 'country-select', :git => 'http://github.com/rails/iso-3166-country-select.git', :submodule => true
 plugin 'formtastic', :git => 'http://github.com/justinfrench/formtastic.git', :submodule => true
-generate 'formtastic_stylesheets'
+generate 'formtastic'
 rm_f 'public/stylesheets/formtastic_changes.css'
 
 
