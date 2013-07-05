78782b9a8168dfd52f8810887ec6803013b53806
Craig Buchek <craig@boochtek.com>
2013-07-05 15:49:33 -0500

    Update application_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,6 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  # Fall back to app/views/default if view file cannot be found in its expected location.
-  include BoochTek::Rails::DefaultViews rescue nil
-
   # Define the default layout.
   layout 'application'
 
@@ -14,21 +11,7 @@ class ApplicationController < ActionController::Base
   # Add CSRF protection, by adding hidden fields to forms, containing a token.
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-  # Ensure that sensitive data doesn't get logged.
-  filter_parameter_logging :password, :password_confirmation, :confirm_password, :ssn, :social_security_number, :credit_card, :credit_card_number, :cvv, :cvv2
-
-  # Activate exception handlers, if we've got them, and we're in production.
-  if ['production', 'staging'].include? Rails.env
-    if const_defined?('HoptoadNotifier') and Hoptoad.api_key
-      include HoptoadNotifier::Catcher
-    end
-    if const_defined?('ExceptionNotifiable')
-      include ExceptionNotifiable
-      ExceptionNotifier.exception_recipients = EMAIL_EXCEPTIONS_TO
-    end
-  end
-
-  # Special case handling for RecordNotFound exceptions. We don't want to send emails for these!
+  # Special case handling for RecordNotFound exceptions. We don't want to be notified of these exceptions.
   def rescue_action(exception)
     case exception
     when ActiveRecord::RecordNotFound
